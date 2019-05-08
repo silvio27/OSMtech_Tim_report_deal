6f35629e36467221febb6c0b550f37599d5d3fc3
silvo <sunzhongshanzpmc@yeah.net>
2019-05-08 20:44:37 +0800

    重命名后导出一份文件及路径清单

--- renamedemo01.py
+++ renamedemo01.py
@@ -4,7 +4,7 @@ import os
 import re
 
 def rename_001(path,folder):
-
+    path1 = path
     path = path + folder +'/'
     f = os.listdir(path)
     n = 0
@@ -21,6 +21,9 @@ def rename_001(path,folder):
             print('error')
 
         os.renames(path+f[n], path+newname)
+        #将文件路径导出到dir
+        add_file_path(path1, 'dir', path+newname)
+
         n += 1
     print('ALL ' + str(n) + ' Done')
     print(j)
@@ -40,9 +43,17 @@ def all_folder_rename_001(path):
         folder = n
         rename_001(path, folder)
 
+def add_file_path(path, filename, content):
+    #可以增加判断是追加还是重新写入
+    fo = open(path + filename + ".txt", "a+")
+    fo.write(content + '\n')
+    fo.close()
+
 
 
 if __name__=="__main__":
-    path = r'C:\Users\Silvio\Desktop\四川省成都市青白江区2019-SC-024\待整理2'
+    path = r'C:\Users\Silvio\Desktop\四川省成都市青白江区2019-SC-024\待整理'
+    #path1 = r'C:/Users/Silvio/Desktop/四川省成都市青白江区2019-SC-024/'
     all_folder_rename_001(path)
+
 
